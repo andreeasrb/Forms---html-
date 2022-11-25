2715df04dcc47b48bb91f8a205b0a23cd89fb8d1
andreeasrb <114910481+andreeasrb@users.noreply.github.com>
2022-11-25 13:26:38 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# express-exercise
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/express-simple-dxja4a)
---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const port = 3010;
+const path = require('path');
+
+app.use(express.static('static'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve('pages/index.html'));
+});
+
+app.get('/users', (req, res) => {
+  res.sendFile(path.resolve('pages/users.html'));
+});
+
+app.get('/test', (req, res) => {
+  console.log(req.query);
+  let queryParams = Object.entries(req.query).reduce(
+    (acc, [key, value]) => `${acc} <p>${key} = ${value}</p>`,
+    ''
+  );
+  res.set('Content-Type', 'text/html');
+  res.send(Buffer.from(queryParams));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
